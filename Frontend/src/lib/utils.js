099f0ea8685837755d0d9636388c9f2085044891
Sushil Dubey <sushilanildubey@gmail.com>
2026-03-01 17:12:42 +0530

    Utils

---
+++ Frontend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(amount);
+}
